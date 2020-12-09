fefdcf99ba02ac55dd11902bd54d12d499434c39
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-12-09 10:55:03 -0300

    Add presentation slides

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const compression = require("compression");
+const path = require("path");
 
 const app = express();
 
 app.use(compression());
 app.use(express.static("public"));
 
+app.get("/slides", (req, res) => {
+  res.sendFile(path.resolve("public/slides.html"));
+});
+
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.resolve("public/index.html"));
 });
 
 module.exports = app;
